31254a2e6283aca0adec08cc48084d126dea9d17
Sss3195 <s20171103195@danqianfang.local>
2018-11-02 19:26:33 +0800

    Initial Commit

---
+++ J_sj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  J_sj
+//
+//  Created by s20171103195 on 2018/11/2.
+//  Copyright © 2018 s20171103195. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
